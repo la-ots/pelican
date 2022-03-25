90e19afe8f979c7a1edba1239aebb96a4982dd8b
Charles Ian Doucet <cdoucet478@gmail.com>
2022-03-25 14:23:22 -0500

    placing in new constructor processes for popovers and tooltips.

--- docs/js/default.js
+++ docs/js/default.js
@@ -140,10 +140,12 @@ function topFunction() {
 
 /*global $*/
 
-$(function () {
-  $("[data-toggle='popover']").popover();
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
 });
 
-$(function () {
-  $("[data-toggle='tooltip']").tooltip();
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
 });
