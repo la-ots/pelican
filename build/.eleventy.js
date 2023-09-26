ae194da29ba1ff751b171b3a47fa12ce9471969b
James Alt <james.alt@la.gov>
2023-09-26 16:08:39 -0500

    Removing debugging logs

--- build/.eleventy.js
+++ build/.eleventy.js
@@ -15,8 +15,6 @@ const { EleventyHtmlBasePlugin } = require("@11ty/eleventy")
 module.exports = (eleventyConfig) => {
   const isProduction = process.env.ELEVENTY_ENV === "production";
 
-  console.log(process.env)
-
   eleventyConfig.addPlugin(pluginToc, {
     wrapper: 'div'
   })
