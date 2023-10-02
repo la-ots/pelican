4e12ffd69cc9893d6b0edbdf1647e9c0581ae95f
James Alt <james.alt@la.gov>
2023-10-02 15:09:03 -0500

    Updates to formatters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,7 @@
 module.exports = {
   root: true,
-  env: {
-    es6: true,
-    node: true,
-    browser: true,
-  },
   extends: ["@la-ots/eslint-config", "prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-    ecmaVersion: 2017,
-    sourceType: "module",
+  globals: {
+    bootstrap: "readonly",
   },
 };
--- docs/js/default.js
+++ docs/js/default.js
@@ -157,18 +157,16 @@ function topFunction() {
   }
 }
 
-/*global $*/
-
 var popoverTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="popover"]'),
 );
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl);
 });
 
 var tooltipTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="tooltip"]'),
 );
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl);
 });
